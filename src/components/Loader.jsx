57a1d69edb1f4f93e1b401da2415430798d66a7f
Mayank171006 <mayankkumarjbb@gmail.com>
2026-06-22 21:30:03 +0530

    Add Loading State

---
+++ src/components/Loader.jsx
@@ -0,0 +1,9 @@
+const Loader = () => {
+  return (
+    <div className="text-center">
+      <div className="spinner-border" role="status">
+      </div>
+    </div>
+  );
+};
+export default Loader;
